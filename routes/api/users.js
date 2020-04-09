9b7d42fbc062f7a38cde64497f6adc638f7ca905
Mridul Gogia <gogiamridul@gmail.com>
2020-04-10 00:11:36 +0530

    updated auth# with '#' will be ignored, and an empty message aborts the commit.

--- routes/api/users.js
+++ routes/api/users.js
@@ -69,7 +69,7 @@ router.post('/login', (req, res) => {
     .then( user => {
         if(!user) {
             errors.email = 'User not found';
-            return res.status(404).json({errors});
+            return res.status(404).json(errors);
         }
         bcrypt.compare(password, user.password)
             .then(isMatch => {
