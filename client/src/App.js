a9bff41fa497438428820aa21bca0b7a1b270495
Mridul Gogia <gogiamridul@gmail.com>
2020-04-12 01:53:23 +0530

    just had dinner

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from './store';
 
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
+
+import PrivateRoute from './components/common/PrivateRoute';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -16,10 +18,13 @@ import Footer from './components/layout/Footer';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
+
 import { setCurrentUser, logoutUser } from './actions/authAction';
+import { clearCurrentProfile } from './actions/profileActions';
 
 class App extends Component {
-
   componentDidMount() {
     if(localStorage.getItem('jwtToken')) {
       setAuthToken(localStorage.getItem('jwtToken'));
@@ -31,6 +36,8 @@ class App extends Component {
       const currentTime = Date.now() / 1000;
       if(decoded.exp < currentTime) {
         store.dispatch(logoutUser());
+
+        store.dispatch(clearCurrentProfile());
 
         this.props.history.push('/login');
       }
@@ -59,6 +66,16 @@ class App extends Component {
                 exact
                 component={Login}
               />
+              <Switch>
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
+              </Switch>
             </div>
             <Footer />
           </div>
